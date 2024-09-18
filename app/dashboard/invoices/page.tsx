57c293a4eaf225a5ad1be9d891d1aecb7e9e62a4
jezour <jezour@livesport.eu>
2024-09-18 20:04:51 +0200

    Prettier formatting. Added dynamic rendering and streaming components.

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,13 +1,13 @@
 export default function Page() {
   return (
-	<div>
-	  <h1 className="text-2xl font-bold">Invoices</h1>
-	  <p className="text-lg">Here are your invoices:</p>
-	  <ul>
-		<li>Invoice 1</li>
-		<li>Invoice 2</li>
-		<li>Invoice 3</li>
-	  </ul>
-	</div>
+    <div>
+      <h1 className="text-2xl font-bold">Invoices</h1>
+      <p className="text-lg">Here are your invoices:</p>
+      <ul>
+        <li>Invoice 1</li>
+        <li>Invoice 2</li>
+        <li>Invoice 3</li>
+      </ul>
+    </div>
   );
 }
